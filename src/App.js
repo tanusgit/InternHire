9f898d1da2ff528843e062763bb74bf86fb6b3ad
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-23 07:29:39 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
     <Menu></Menu>
     <Jobcomponent></Jobcomponent>
-    <Main></Main>
+    
 
     </div>
 
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import Menu from 'Menu';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import Menu from './Menu';
+import { Switch, Route, Router, Redirect } from 'react-router-dom';
 import Jobcomponent from './joblistcomponent';
 
 
-class Main extends component {
+class Main extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            menu : Menu
+        };
     }
 
     render() {
         return (
-
+            <Router>
             <Switch>
                 <Route path="/Menu" component={Menu} />
-                <Route exact path='/joblistcomponent' component={Jobcomponent} />} />
-                <Redirect to="/Menu" />
             </Switch>
+            </Router>
         );
     }
 
